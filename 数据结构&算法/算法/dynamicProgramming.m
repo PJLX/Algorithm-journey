9b21fc3af7110748306fda19bbefd88f1580f02d
李响 <601148234@qq.com>
2018-06-09 17:31:51 +0800

    add readme

--- 数据结构&算法/算法/dynamicProgramming.m
+++ 数据结构&算法/算法/dynamicProgramming.m
@@ -1,54 +1,54 @@
 //
-//  knapsackProblem.m
-//  算法-动态规划
+//  dynamicProgramming.m
+//  算法
 //
-//  Created by 李响 on 2018/5/6.
+//  Created by 李响 on 2018/6/9.
 //  Copyright © 2018年 LiXiang. All rights reserved.
-//  O(n*n)
+//  动态规划 O(n*n)
 
-#import "knapsackProblem.h"
+#import "dynamicProgramming.h"
 
-@interface knapsackProblem ()
-
+@interface dynamicProgramming ()
 @property (nonatomic, strong) NSMutableArray<NSMutableArray<NSNumber*>*> *gridding;
 @property (nonatomic, strong) NSMutableArray *selectedGoods;
 @property (nonatomic, copy) NSDictionary<NSString*, NSNumber*> *graphValue;
 @property (nonatomic, copy) NSDictionary<NSString*, NSNumber*> *graphWeight;
+
 @end
 
-@implementation knapsackProblem
+@implementation dynamicProgramming
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
     
     /*
-            1     2     3     4     5
+     1     2     3     4     5
      a 1  1500  1500  1500  1500  1500
      b 3  1500  1500  2000  3500  3500
-            |
-             -----------------------
-                                    |
+     |
+     -----------------------
+     |
      c 4  1500  1500  2000  5000 -6500
-                                    |
+     |
      d 2  1500  2500  4000  5000  6500
      */
     
     self.graphValue = @{@"a": @1500,
-                            @"b": @2000,
-                            @"c": @5000,
-                            @"d": @2500,
-                            };
+                        @"b": @2000,
+                        @"c": @5000,
+                        @"d": @2500,
+                        };
     self.graphWeight = @{@"a": @1,
-                                  @"b": @3,
-                                  @"c": @4,
-                                  @"d": @2,
-                                  };
-//    NSInteger size = 7;
-//    NSInteger maxValue = [self findMaxminumValueWithKnapsackSize:size graphValue:self.graphValue graphWeight:self.graphWeight];
-//    self.selectedGoods = [NSMutableArray new];
-//    [self findGoodsWithRow:self.graphValue.allKeys.count-1 list:size - 1 maxValue:maxValue];
-//    NSLog(@"%@",self.selectedGoods);
+                         @"b": @3,
+                         @"c": @4,
+                         @"d": @2,
+                         };
+    //    NSInteger size = 7;
+    //    NSInteger maxValue = [self findMaxminumValueWithKnapsackSize:size graphValue:self.graphValue graphWeight:self.graphWeight];
+    //    self.selectedGoods = [NSMutableArray new];
+    //    [self findGoodsWithRow:self.graphValue.allKeys.count-1 list:size - 1 maxValue:maxValue];
+    //    NSLog(@"%@",self.selectedGoods);
     
     
     [self maxsSubSequenceBetweenA:@"foisha" andB:@"fish"];
@@ -56,7 +56,7 @@
 
 /**
  背包问题
-
+ 
  @param size 背包容量
  @param graphValue 商品价值列表
  @param graphWeight 商品重量列表
@@ -139,16 +139,16 @@
 
 /**
  最长/大公共子串
-       f o s h i
-     f
-     i
-     s
-     h
+ f o s h i
+ f
+ i
+ s
+ h
  if a[i] == b[j]
  cell[i][j] = cell[i-1][j-1] + 1;
  else
  cell[i][j] = 0;
-
+ 
  @param a a
  @param b b
  @return 最长/大公共子串
@@ -165,7 +165,7 @@
     
     for (NSUInteger i = 0; i < lengthA; i++) {
         NSMutableArray<NSNumber*> *rowArr = [NSMutableArray new];
-
+        
         for (NSUInteger j = 0; j < lengthB; j++) {
             NSString *subA = [a substringWithRange:NSMakeRange(i, 1)];
             NSString *subB = [b substringWithRange:NSMakeRange(j, 1)];
